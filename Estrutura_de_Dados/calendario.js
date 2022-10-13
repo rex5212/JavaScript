4cf9d47b04ebfe4a8a231c732b54c70d27f8d4c0
unknown <21114290027@academico.iesb.br>
2022-10-13 20:37:31 -0300

    Atualização calendario

--- Estrutura_de_Dados/calendario.js
+++ Estrutura_de_Dados/calendario.js
@@ -1,18 +1,82 @@
+/*
 var calendario = []
 
 for(ano = 1; ano < 2; ano++){ //ano
     calendario[ano] = []
-    for(diaC = 0; diaC < 12; diaC++){ //mes
-        calendario[ano][diaC] = []
-        if (diaC)
-        for(mes = 0; mes < 31; mes++){
-            calendario[ano][diaC][mes] = ano + mes
-        }
-        for(mes = 0; mes < 31; mes++){
-            calendario[ano][diaC][mes] = ano + mes
+
+    for(mes = 0; mes < 12; mes++){ //mes
+        calendario[ano][mes] = []
+
+        for(dia = 0; dia < 30; dia++){
+            
+            calendario[ano][mes][dia] = 1 + dia
         }
     }
 
 }
 
 console.log(calendario)
+*/
+
+
+// professor
+
+let mes = 0
+while(mes < 12){
+
+    switch(mes){
+        case 1:
+            console.log('')
+            break
+        case 2:
+            console.log('')
+            break
+        case 3:
+            console.log('')
+            break
+        case 4:
+            console.log('')
+            break
+        case 5:
+            console.log('')
+            break
+        case 6:
+            console.log('')
+            break
+        case 7:
+            console.log('')
+            break
+        case 8:
+            console.log('')
+            break
+        case 9:
+            console.log('')
+            break
+        case 10:
+            console.log('')
+            break
+        case 11:
+            console.log('')
+            break
+        case 12:
+            console.log('')
+            break
+    }
+
+
+
+    let dia = 1
+
+    console.log('D ', 'S ', 'T ', 'Q ', 'Q ', 'S ', 'S ')
+    
+    for (let linha = 0; linha < 6; linha++){
+        for (let coluna = 2; coluna < 7; coluna++){
+            console.log(dia + " ")
+            dia++
+        }
+
+    }
+
+mes++
+}
+
